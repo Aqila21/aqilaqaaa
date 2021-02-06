5f96b821a718dd14c71a5bdb5487071426fbec69
nashaputri04 <nashacacaa@gmail.com>
2021-02-06 11:36:10 +0700

    first commit

---
+++ aqila05/application/controllers/Crud.php
@@ -0,0 +1,67 @@
+<?php 
+
+
+class Crud extends CI_Controller{
+
+	function __construct(){
+		parent::__construct();		
+		$this->load->model('m_data');
+		$this->load->helper('url');
+
+	}
+
+	function index(){
+		$data['user'] = $this->m_data->tampil_data()->result();
+		$this->load->view('v_tampil',$data);
+	}
+
+	function tambah(){
+		$this->load->view('v_input');
+	}
+
+	function hapus($id){
+		$where = array('id' => $id);
+		$this->m_data->hapus_data($where,'users');
+		redirect('crud/index');
+	}
+ 
+	function edit($id){
+		$where = array('id' => $id);
+		$data['user'] = $this->m_data->edit_data($where,'users')->result();
+		$this->load->view('v_edit',$data);
+	}
+ 
+	function tambah_aksi(){
+		$nama = $this->input->post('nama');
+		$alamat = $this->input->post('alamat');
+		$pekerjaan = $this->input->post('pekerjaan');
+
+		$data = array(
+			'nama' => $nama,
+			'alamat' => $alamat,
+			'pekerjaan' => $pekerjaan
+			);
+		$this->m_data->input_data($data,'users');
+		redirect('crud/index');
+	}
+
+	function update(){
+		$id = $this->input->post('id');
+		$nama = $this->input->post('nama');
+		$alamat = $this->input->post('alamat');
+		$pekerjaan = $this->input->post('pekerjaan');
+	
+		$data = array(
+			'nama' => $nama,
+			'alamat' => $alamat,
+			'pekerjaan' => $pekerjaan
+		);
+	
+		$where = array(
+			'id' => $id
+		);
+	
+		$this->m_data->update_data($where,$data,'users');
+		redirect('crud/index');
+	}
+}
